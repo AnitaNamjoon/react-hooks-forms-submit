c50a1c36734f162c44e211831b14b838a53c81b2
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-07-28 14:01:18 +0300

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 
-function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+function Form() {
+  // State variables for form inputs
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  // State variable for holding form submission data
+  const [submittedData, setSubmittedData] = useState([]);
+  // State variable for holding form validation errors
+  const [errors, setErrors] = useState([]);
 
+  // Event handlers for input changes
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
   }
@@ -12,12 +18,63 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  // Form submission handler
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // Basic input validation
+    if (firstName.trim() === "" || lastName.trim() === "") {
+      setErrors(["Both first name and last name are required!"]);
+    } else {
+      // Clear errors if the inputs are valid
+      setErrors([]);
+
+      // Construct form data and update the submittedData state
+      const formData = { firstName: firstName, lastName: lastName };
+      setSubmittedData([...submittedData, formData]);
+
+      // Clear input fields after submission
+      setFirstName("");
+      setLastName("");
+    }
+  }
+
+  // JSX to display form, errors, and submitted data
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={handleFirstNameChange}
+          value={firstName}
+          placeholder="First Name"
+        />
+        <input
+          type="text"
+          onChange={handleLastNameChange}
+          value={lastName}
+          placeholder="Last Name"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {/* Conditionally render error messages */}
+      {errors.length > 0 &&
+        errors.map((error, index) => (
+          <p key={index} style={{ color: "red" }}>
+            {error}
+          </p>
+        ))}
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
   );
 }
 
